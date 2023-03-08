6f457387251e488e0bbc552ece56b952341d74a1
Otto Ahoniemi <otto.ahoniemi@mindler.fi>
2023-03-08 16:39:10 +0200

    v2

--- src/client.ts
+++ src/client.ts
@@ -1,65 +1,128 @@
-import type {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  CreateTableCommand,
+  CreateTableCommandOutput,
+  DeleteTableCommand,
+  DeleteTableCommandOutput,
   DynamoDBClient,
-  DynamoDBClientResolvedConfig,
-  ServiceInputTypes as __ServiceInputTypes,
-  ServiceOutputTypes as __ServiceOutputTypes,
+  DynamoDBClientConfig,
 } from "@aws-sdk/client-dynamodb";
-import { Client as __Client } from "@aws-sdk/smithy-client";
-import type { HttpHandlerOptions as __HttpHandlerOptions } from "@aws-sdk/types";
-import type {
-  marshallOptions,
-  unmarshallOptions,
-} from "@aws-sdk/util-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  GetCommand,
+  GetCommandOutput,
+  PutCommand,
+  PutCommandOutput,
+  type TranslateConfig,
+} from "@aws-sdk/lib-dynamodb";
+import type { HttpHandlerOptions } from "@aws-sdk/types";
 import type { z } from "zod";
+import type { Spread } from "type-fest";
 
-import type { GetCommandInput, GetCommandOutput } from "./commands/GetCommand";
+// TS doesn't allow classes to override static methods of base classes: hence
+// omitting "from" method her
+// https://github.com/microsoft/TypeScript/issues/4628#issuecomment-1147905253
+type DynamoDBDocumentClientConstructorParams = [
+  client: DynamoDBClient,
+  translateConfig?: TranslateConfig
+];
+// @ts-expect-error DynamoDBDocumentClient constructor is protected, but needs to used
+// here to omit "from"
+const DynamoDBDocumentClientWithoutFrom: (new (
+  ...args: DynamoDBDocumentClientConstructorParams
+) => DynamoDBDocumentClient) &
+  Omit<typeof DynamoDBDocumentClient, "from"> = DynamoDBDocumentClient;
 
-export type ServiceInputTypes = __ServiceInputTypes | GetCommandInput;
+// TODO: Figure out a way to overload only the modified stuff, no other fields
 
-export type ServiceOutputTypes<Schema extends z.AnyZodObject> =
-  | __ServiceOutputTypes
-  | GetCommandOutput<Schema>;
+type ZynamoCommand =
+  | GetCommand
+  | PutCommand
+  | CreateTableCommand
+  | DeleteTableCommand;
+type ZynamoOutputs =
+  | GetCommandOutput
+  | PutCommandOutput
+  | CreateTableCommandOutput
+  | DeleteTableCommandOutput;
 
-export type TranslateConfig = {
-  marshallOptions?: marshallOptions;
-  unmarshallOptions?: unmarshallOptions;
-};
+// TODO: what
+// type ValidPutCommandInput<Schema extends z.AnyZodObject> = Spread<
+//   Omit<PutCommandInput, "Item">,
+//   { Item: z.input<Schema> }
+// >;
 
-export type DynamoDBDocumentClientResolvedConfig =
-  DynamoDBClientResolvedConfig & {
-    translateConfig?: TranslateConfig;
-  };
+// const ValidPutCommand: (new (
+//   ...args: ConstructorParameters<typeof PutCommand>
+// ) => PutCommand) &
+//   Omit<typeof PutCommand, "input"> & { input: ValidPutCommandInput } =
+//   PutCommand;
 
-export class DynamoDBDocumentClient<
+export class ZynamoClient<
   Schema extends z.AnyZodObject
-> extends __Client<
-  __HttpHandlerOptions,
-  ServiceInputTypes,
-  ServiceOutputTypes<Schema>,
-  DynamoDBDocumentClientResolvedConfig
-> {
-  override readonly config: DynamoDBDocumentClientResolvedConfig;
+> extends DynamoDBDocumentClientWithoutFrom {
+  schema: Schema;
 
   protected constructor(
-    client: DynamoDBClient,
-    translateConfig?: TranslateConfig
+    schema: Schema,
+    ...superParams: DynamoDBDocumentClientConstructorParams
   ) {
-    super(client.config);
-    this.config = client.config;
-    if (translateConfig) {
-      this.config.translateConfig = translateConfig;
+    super(...superParams);
+    this.schema = schema;
+  }
+
+  // === Implementations for overridden functionalities
+  async handleGetCommand(command: GetCommand) {
+    const result = await super.send(command);
+    if (result.Item) {
+      const parsed = this.schema.parse(result.Item);
+      return {
+        ...result,
+        Item: parsed.data,
+      };
     }
-    this.middlewareStack = client.middlewareStack;
+    return result;
   }
 
+  async handlePutCOmmand(command: PutCommand) {
+    this.schema.parse(command.input.Item);
+    return super.send(command);
+  }
+
+  // === Overloads for overridden functionalities
+  override send(
+    command: GetCommand
+  ): Promise<
+    Spread<Omit<GetCommandOutput, "Item">, { Item?: z.output<Schema> }>
+  >;
+
+  override send(command: PutCommand): Promise<PutCommandOutput>;
+
+  // === Not overridden but I want to keep these usable
+  override send(command: CreateTableCommand): Promise<CreateTableCommandOutput>;
+
+  override send(command: DeleteTableCommand): Promise<DeleteTableCommandOutput>;
+
+  override send(
+    command: ZynamoCommand,
+    options?: HttpHandlerOptions
+  ): Promise<ZynamoOutputs> {
+    if (command instanceof GetCommand) {
+      return this.handleGetCommand(command);
+    }
+    if (command instanceof PutCommand) {
+      return this.handlePutCOmmand(command);
+    }
+    return super.send(command as any, options) as any;
+  }
+
+  // Create dynamoDBClient and pass it to ZynamoClient like with documentClient
   static from<Schema extends z.AnyZodObject>(
-    client: DynamoDBClient,
+    schema: Schema,
+    config: DynamoDBClientConfig,
     translateConfig?: TranslateConfig
   ) {
-    return new DynamoDBDocumentClient<Schema>(client, translateConfig);
-  }
-
-  override destroy(): void {
-    // A no-op, since client is passed in constructor
+    const dynamoDBClient = new DynamoDBClient(config);
+    return new ZynamoClient<Schema>(schema, dynamoDBClient, translateConfig);
   }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -2,48 +2,39 @@ import { describe, it, expect, beforeAll, afterAll } from "vitest";
 import { GenericContainer, type StartedTestContainer } from "testcontainers";
 import {
   CreateTableCommand,
-  CreateTableCommandInput,
+  DeleteTableCommand,
   type DynamoDBClientConfig,
 } from "@aws-sdk/client-dynamodb";
 import { z } from "zod";
-import type { DynamoDBDocumentClient } from "../src/client";
-import { createClient } from "../src";
-import { createCommandFactory } from "../src/commands/commandFactory";
+import { GetCommand, PutCommand } from "@aws-sdk/lib-dynamodb";
+import { generateMock } from "@anatine/zod-mock";
+import { ZynamoClient } from "../src";
 
 const dynamodbPort = 8000;
-const tableName = "test-table";
-const testSchema = z.object({
-  foo: z.string(),
-  bar: z.number(),
-  cat: z.boolean(),
-  dog: z.string().optional(),
-  fish: z.string().nullable(),
-  fishes: z.array(z.string()),
-});
 
-const commandFactory = createCommandFactory(testSchema);
+// TODO: logger
+
+// TODO: handle that tests don't rely on each other
 
 describe("zynamo", () => {
   let container: StartedTestContainer;
   let containerUrl: string;
-  let client: DynamoDBDocumentClient<typeof testSchema>;
+  let config: DynamoDBClientConfig;
 
   beforeAll(async () => {
     console.log("Setting up dynamodb container");
     container = await new GenericContainer("amazon/dynamodb-local")
       .withExposedPorts(dynamodbPort)
       .start();
+
     const logs = await container.logs();
     logs.on("err", (line) => console.error("[Container error]:", line));
+
     containerUrl = `http://${container.getHost()}:${container.getMappedPort(
       dynamodbPort
     )}`;
-    console.log(
-      `Dynamodb container with id ${container.getId()} and name ${container.getName()} running on ${containerUrl}}`
-    );
 
-    console.log("Setting up dymamodb client with schema");
-    const config: DynamoDBClientConfig = {
+    config = {
       endpoint: containerUrl,
       region: "us-east-1",
       credentials: {
@@ -51,64 +42,180 @@ describe("zynamo", () => {
         secretAccessKey: "xxxxx",
       },
     };
-    client = createClient(config, testSchema);
-    console.log("Dynamodb client created");
 
-    console.log("Setting up table");
-    const params: CreateTableCommandInput = {
-      AttributeDefinitions: [
-        {
-          AttributeName: "Stuff",
-          AttributeType: "S",
-        },
-      ],
-      KeySchema: [
-        {
-          AttributeName: "Stuff",
-          KeyType: "HASH",
-        },
-      ],
-      ProvisionedThroughput: {
-        ReadCapacityUnits: 1,
-        WriteCapacityUnits: 1,
-      },
-      TableName: tableName,
-    };
-    const response = await client.send(new CreateTableCommand(params));
-    expect(response.$metadata.httpStatusCode).toBe(200);
+    console.log(
+      `Dynamodb container with id ${container.getId()} and name ${container.getName()} running on ${containerUrl}}`
+    );
   });
 
   afterAll(async () => {
     await container.stop();
   });
 
-  // it("puts data normally", async () => {
-  //   const params: PutCommandInput = {
-  //     TableName: tableName,
-  //     Item: {
-  //       Stuff: "ID_1",
-  //       foo: "foo",
-  //       bar: 1,
-  //       cat: true,
-  //       fish: null,
-  //       fishes: ["a", "b", "c"],
-  //     },
-  //   };
-  //   const data = await client.send(new PutCommand(params));
-  //   console.log(data);
-  //   expect(data).toBeDefined();
-  // });
+  describe("with schema", () => {
+    // TODO: better tests with some property based hassle
+    const schema = z.object({
+      foo: z.string(),
+      bar: z.number(),
+      foobar: z.boolean(),
+      foobarbaz: z.string().optional(),
+      foobarbazqux: z.string().nullable(),
+      foobaaars: z.array(z.string()),
+      fooBarMap: z.map(
+        z.string(),
+        z.object({ jou: z.number(), jii: z.string() })
+      ),
+    });
+    let client: ZynamoClient<typeof schema>;
 
-  it("gets data normally", async () => {
-    const data = await client.send(
-      commandFactory.createGetCommand({
-        TableName: tableName,
-        Key: {
-          Stuff: "ID_1",
-        },
-      })
-    );
-    console.log(data);
-    expect(data).toBeDefined();
+    it("client can be created", () => {
+      client = ZynamoClient.from(schema, config);
+      expect(client).toBeDefined();
+      // TODO: test that client doesn't accept unsuitable schema
+    });
+
+    it("client can create a table", async () => {
+      // TODO: test that client accepts only table names defined in schema (later when
+      // schemas handle multiple tables & sort keys etc)
+      const response = await client.send(
+        new CreateTableCommand({
+          AttributeDefinitions: [
+            {
+              AttributeName: "Swag",
+              AttributeType: "S",
+            },
+          ],
+          KeySchema: [
+            {
+              AttributeName: "Swag",
+              KeyType: "HASH",
+            },
+          ],
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1,
+          },
+          TableName: "LitSwagTable",
+        })
+      );
+      expect(response.$metadata.httpStatusCode).toBe(200);
+      await client.send(new DeleteTableCommand({ TableName: "LitSwagTable" }));
+    });
+
+    describe("putting data to table", async () => {
+      const tableForPut = "FooTableToPut";
+      beforeAll(async () => {
+        await client.send(
+          new CreateTableCommand({
+            AttributeDefinitions: [
+              {
+                AttributeName: "foo",
+                AttributeType: "S",
+              },
+            ],
+            KeySchema: [
+              {
+                AttributeName: "foo",
+                KeyType: "HASH",
+              },
+            ],
+            ProvisionedThroughput: {
+              ReadCapacityUnits: 1,
+              WriteCapacityUnits: 1,
+            },
+            TableName: tableForPut,
+          })
+        );
+      });
+      it("succeeds with valid input", async () => {
+        const input = generateMock(schema);
+        const response = await client.send(
+          new PutCommand({
+            TableName: tableForPut,
+            Item: input,
+          })
+        );
+        expect(response.$metadata.httpStatusCode).toBe(200);
+      });
+      it("fails with not valid input", async () => {
+        expect(
+          client.send(
+            new PutCommand({
+              TableName: tableForPut,
+              Item: {
+                swag: "littinen",
+              },
+            })
+          )
+        ).rejects.toThrowErrorMatchingSnapshot();
+      });
+    });
+
+    describe("getting data from table", async () => {
+      const tableForGet = "FooTableForGet";
+      const fooValue = "1";
+      let input: z.infer<typeof schema>;
+      beforeAll(async () => {
+        await client.send(
+          new CreateTableCommand({
+            AttributeDefinitions: [
+              {
+                AttributeName: "foo",
+                AttributeType: "S",
+              },
+            ],
+            KeySchema: [
+              {
+                AttributeName: "foo",
+                KeyType: "HASH",
+              },
+            ],
+            ProvisionedThroughput: {
+              ReadCapacityUnits: 1,
+              WriteCapacityUnits: 1,
+            },
+            TableName: tableForGet,
+          })
+        );
+        // add input
+        input = generateMock(schema);
+        input.foo = fooValue;
+        await client.send(
+          new PutCommand({
+            TableName: tableForGet,
+            Item: input,
+          })
+        );
+      });
+      it("returns valid stuff", async () => {
+        // TODO: fix map - object conversion
+        const response = await client.send(
+          new GetCommand({
+            TableName: tableForGet,
+            // TODO: validate key
+            Key: {
+              foo: fooValue,
+            },
+          })
+        );
+        expect(response.$metadata.httpStatusCode).toBe(200);
+        expect(response.Item).toMatchObject(input);
+      });
+      it("doesn't fail when item not in table", async () => {
+        const response = await client.send(
+          new GetCommand({
+            TableName: tableForGet,
+            // TODO: validate key
+            Key: {
+              foo: "nonValidValue",
+            },
+          })
+        );
+        expect(response.$metadata.httpStatusCode).toBe(200);
+        expect(response.Item).toBeUndefined();
+      });
+      // TODO: test that we error for data integrity when data doesn't match schema
+      // content
+    });
   });
 });
